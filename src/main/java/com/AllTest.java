d55dc95cd15ec4f2d73573173547c6e41cd87ca8
Abbas H <abbasali9960@gmail.com>
2022-09-15 19:46:23 +0530

    updated test cases

--- src/main/java/com/AllTest.java
+++ src/main/java/com/AllTest.java
@@ -19,63 +19,71 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.Parameters;
 
 public class AllTest {
-	private WebDriver driver = null;
-	
-	@Test(priority = 1)
-	@Parameters({ "myURL", "myTitle" })
-	public void checkHopePageUp(String myURL, String myTitle) {	    
-		driver.get(myURL);	
-		String title = driver.getTitle();
-		Assert.assertTrue(title.contains(myTitle));
-		System.out.println("Store Page is up");	
-		System.out.println("**************");	
-		 System.out.println('\n');
-		
-	}
-    @Test(priority = 2)
-	@Parameters({"searchTerm"})
-	public void SearchTerm(String searchTerm ) {
-    	driver.manage().window().maximize() ;
-		pageObjects pg = new pageObjects();
-		pg.pageWebDriver(driver);
-	   
-	   //set Search term
-	   pg.setSearchTerm(searchTerm);
-	   pg.clickSearchButton();
-	   assert(pg.getSearchResult().toString().contains(searchTerm));
-	   System.out.println("Validated Searched Result");   
-	   System.out.println("**************");	
-	   System.out.println('\n');
-	  // driver =pg.getDriver();
-	
-	}
-    @Test(priority = 3)
-   	@Parameters({"myURL","myDress"})
-   	public void selectCategoryWomen(String myURL,String myDress) { 
-    	
-   		pageObjects pg1 = new pageObjects();
-   		pg1.pageWebDriver(driver);
-   	    pg1.getUrl(myURL);
-   	   //click women category
-   	   pg1.clickWomenCategory();
-   	   System.out.println("Women Category Clicked");   
-   	   pg1.clickTopCategory();
-   	   pg1.clickColor();
-    	 assert(pg1.getSearchResult().toString().contains(myDress));
-    	 System.out.println("Validated Result after Selection");	
-    	   System.out.println("**************");	
-    	   System.out.println('\n');
-   	}
-	
-    @BeforeTest
-	public void beforeTest() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-		driver = new ChromeDriver();
-	}
+    private WebDriver driver = null;
 
-	@AfterTest
-	public void afterTest() {
-       driver.quit();
-	}
+    @Test(priority = 1)
+    @Parameters({
+        "myURL",
+        "myTitle"
+    })
+    public void checkHopePageUp(String myURL, String myTitle) {
+        driver.get(myURL);
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains(myTitle));
+        System.out.println("Store Page is up");
+        System.out.println("**************");
+        System.out.println('\n');
+
+    }
+    @Test(priority = 2)
+    @Parameters({
+        "searchTerm"
+    })
+    public void SearchTerm(String searchTerm) {
+        driver.manage().window().maximize();
+        pageObjects pg = new pageObjects();
+        pg.pageWebDriver(driver);
+
+        //set Search term
+        pg.setSearchTerm(searchTerm);
+        pg.clickSearchButton();
+        assert(pg.getSearchResult().toString().contains(searchTerm));
+        System.out.println("Validated Searched Result");
+        System.out.println("**************");
+        System.out.println('\n');
+        // driver =pg.getDriver();
+
+    }
+    @Test(priority = 3)
+    @Parameters({
+        "myURL",
+        "myDress"
+    })
+    public void selectCategoryWomen(String myURL, String myDress) {
+
+        pageObjects pg1 = new pageObjects();
+        pg1.pageWebDriver(driver);
+        pg1.getUrl(myURL);
+        //click women category
+        pg1.clickWomenCategory();
+        System.out.println("Women Category Clicked");
+        pg1.clickTopCategory();
+        pg1.clickColor();
+        assert(pg1.getSearchResult().toString().contains(myDress));
+        System.out.println("Validated Result after Selection");
+        System.out.println("**************");
+        System.out.println('\n');
+    }
+
+    @BeforeTest
+    public void beforeTest() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
 
 }
